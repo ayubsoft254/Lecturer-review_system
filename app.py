5fe742524755f61cbbe5905bc929eb4b92210765
ayubsoft254 <henryoayub15@gmail.com>
2023-12-10 06:16:08 +0300

    Initial commit with app.py

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Configure the SQLite database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize SQLAlchemy with the app
+db = SQLAlchemy(app)
+
+# Define the Feedback model
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    student = db.Column(db.String(200))
+    student_id = db.Column(db.String(200), unique=True)
+    lecturer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, student, student_id, lecturer, rating, comments):
+        self.student = student
+        self.student_id = student_id
+        self.lecturer = lecturer
+        self.rating = rating
+        self.comments = comments
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/results')
+def results():
+    # Query the database to get results (assuming Feedback is the model)
+    results = db.session.query(Feedback.lecturer,
+                               db.func.avg(Feedback.rating).label('average_rating'),
+                               db.func.count(Feedback.rating).label('num_ratings')) \
+                        .group_by(Feedback.lecturer).all()
+
+    # Create a dictionary to store the results
+    results_dict = {row[0]: {'average_rating': row[1], 'num_ratings': row[2]} for row in results}
+
+    return render_template('results.html', results=results_dict)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        student = request.form['student']
+        student_id = request.form['student_id']
+        lecturer = request.form['lecturer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+
+        # Validate the input data
+        if not student or not lecturer or not rating:
+            return render_template('index.html', message='Please enter required fields')
+
+        # Check if the student has already submitted feedback for the lecturer
+        existing_feedback = Feedback.query.filter_by(student_id=student_id, lecturer=lecturer).first()
+        if existing_feedback:
+            return render_template('index.html', message='You have already submitted feedback for this lecturer')
+
+        # Save the feedback to the database
+        feedback = Feedback(student=student, student_id=student_id, lecturer=lecturer, rating=rating, comments=comments)
+        db.session.add(feedback)
+        db.session.commit()
+
+        # Redirect the user to the success page
+        return render_template('success.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        # Create the SQLite database file if not exists and initialize tables
+        db.create_all()
+    app.run()
